e182b7cc0c884881f34f2e409c9f8a251ff28641
QED0711 <qdizon@gmail.com>
2020-10-09 19:50:44 -0400

    test setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {TEST_XML} from "./test";
+
+const opensheetmusicdisplay = require("opensheetmusicdisplay")
+
 function App() {
+
+  useEffect(() => {
+    console.log(opensheetmusicdisplay)
+
+    const osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("sheetmusic")
+
+    osmd.load(TEST_XML)
+      .then(() => {
+        osmd.render()
+        osmd.cursor.show()
+        osmd.cursor.next()
+
+        debugger
+      })
+    // console.log(osmd)
+
+  
+    
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="sheetmusic"></div>
     </div>
   );
 }
