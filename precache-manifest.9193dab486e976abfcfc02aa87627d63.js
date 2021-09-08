44c81f0654f7ccdd7ed358787b0a99e5b814baff
QED0711 <qdizon@gmail.com>
2021-09-08 05:32:34 -0400

    Updates

---
+++ precache-manifest.9193dab486e976abfcfc02aa87627d63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71310baf6280a01279876beb30b84844",
+    "url": "/osmd-test/index.html"
+  },
+  {
+    "revision": "53a76b6337c1852008ae",
+    "url": "/osmd-test/static/css/main.606632c3.chunk.css"
+  },
+  {
+    "revision": "fd0222556e228ff1d100",
+    "url": "/osmd-test/static/js/2.71b6497c.chunk.js"
+  },
+  {
+    "revision": "e12c96767c35f7f78d5052ffe2d2cb7f",
+    "url": "/osmd-test/static/js/2.71b6497c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a76b6337c1852008ae",
+    "url": "/osmd-test/static/js/main.3ba8b716.chunk.js"
+  },
+  {
+    "revision": "02b2270925a249851f3f",
+    "url": "/osmd-test/static/js/runtime-main.122b3c8e.js"
+  },
+  {
+    "revision": "cf9b03d2d1282e6c64aba96dce72501f",
+    "url": "/osmd-test/static/media/A_min9.cf9b03d2.wav"
+  },
+  {
+    "revision": "970b368fc30b682060d0f628a6cacf7a",
+    "url": "/osmd-test/static/media/C_major.970b368f.wav"
+  }
+]);
