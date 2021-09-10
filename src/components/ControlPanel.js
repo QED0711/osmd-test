236919bbeabc6072b74a91eabb71e36dd54d8ba6
QED0711 <qdizon@gmail.com>
2021-09-10 05:44:23 -0400

    updated 12_bb, added sus_slash

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -4,10 +4,12 @@ import { useParams } from 'react-router';
 import aMinResults from '../results/A_min9.json'
 import cMajResults from '../results/C_major.json'
 import bluesResults from '../results/12_bb.json'
+import susSlashResults from '../results/sus_slash.json'
 
 import aMinAudio from '../audio/A_min9.wav'
 import cMajAudio from '../audio/C_major.wav'
 import bluesAudio from "../audio/12_bar_blues_audio.wav"
+import susSlashAudio from '../audio/sus_slash.wav'
 
 const SCORES = {
     "a_min": {
@@ -28,6 +30,14 @@ const SCORES = {
         ],
         results: bluesResults,
         audio: bluesAudio
+    },
+    "sus_slash": {
+        url: "https://raw.githubusercontent.com/QED0711/osmd-test/feature/demo_results/src/scores/Sus_Slash.xml",
+        indexTransform: [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 24, 25, 26, 27, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 64, 65, 66, 67, 68, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 114, 115, 116, 117, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174
+        ],
+        results: susSlashResults,
+        audio: susSlashAudio
     }
 }
 
